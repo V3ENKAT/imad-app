7fb601859142084e06486e845c2c70a9fa995bfa
V3ENKAT <30932829+V3ENKAT@users.noreply.github.com>
2017-08-14 22:26:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,5 +10,5 @@ function moveRight(){
     marginLeft=marginLeft+10;
     img.style.marginLeft=marginLeft +'px'}
 img.onclick=function(){
-    var interval = setInterval(moveRight , 100);
+    var interval = setInterval(moveRight , 50);
 }
