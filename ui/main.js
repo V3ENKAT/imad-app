a07a4f8416e4c41599539e7f465ee42a3ab209a9
V3ENKAT <30932829+V3ENKAT@users.noreply.github.com>
2017-08-14 22:14:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,9 @@ console.log('Loaded!');
 //Change the text of the main text div
 var element = document.getElementById('main-text');
 element.innerHTML = 'New value';
+
+//move the image
+var img = document.getElementById('madi');
+img.onclick=function(){
+    img.style.marginLeft="100px";
+}
