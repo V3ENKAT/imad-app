81de950b045e35047da44963a76eedafa46c5894
V3ENKAT <30932829+V3ENKAT@users.noreply.github.com>
2017-08-15 09:38:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,32 @@
-console.log('Loaded!');
+//console.log('Loaded!');
 //Change the text of the main text div
-var element = document.getElementById('main-text');
-element.innerHTML = '---';
+//var element = document.getElementById('main-text');
+//element.innerHTML = '---';
 
 //move the image
-var img = document.getElementById('madi');
-var marginLeft=0;
-function moveRight(){
-    marginLeft=marginLeft+1;
-    img.style.marginLeft=marginLeft +'px'}
-img.onclick=function(){
-    var interval = setInterval(moveRight , 50);
+//var img = document.getElementById('madi');
+//var marginLeft=0;
+//function moveRight(){
+//    marginLeft=marginLeft+1;
+//   img.style.marginLeft=marginLeft +'px'}
+//img.onclick=function(){
+//    var interval = setInterval(moveRigh//, 50);    }
+
+// COUNTER CODE
+
+var button = document.getElementById("count")
+button.onclick=function (){
+    
+    //Make a request to counter endpoint
+    
+    //Capture the response and store it in a variable
+    
+    //Render the variable in the correct span
+    counter = counter+1;
+    var span= document.getElementById('count');
+    span.innerHTML=counter.toString();
+    
 }
+
+
+
