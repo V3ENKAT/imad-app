da434266b92b8dc570367d64f75c8efbc454566e
V3ENKAT <30932829+V3ENKAT@users.noreply.github.com>
2017-08-15 10:47:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,7 +24,7 @@ button.onclick=function (){
  
      //Capture the response and store it in a variable
      request.onreadystatechange = function (){
-         if (request.readystate === XMLHttpRequest.DONE){
+         if (request.readyState === XMLHttpRequest.DONE){
              //Take some action
                    if(request.status === 200){  
                         var counter = request.responseText
@@ -33,7 +33,7 @@ button.onclick=function (){
                      }
          //Not done yet
              }
-    }   
+             }   
    
     //Make the request
     request.open('GET', 'http://sanvenkat21072017.imad.hasura-app.io/counter',true);     
