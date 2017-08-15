ff1936c94daf2d0a36bd0d744ed028e68595f845
V3ENKAT <30932829+V3ENKAT@users.noreply.github.com>
2017-08-15 10:15:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,16 +19,27 @@ var counter = 0;
 
 button.onclick=function (){
     
-    //Make a request to counter endpoint
+    //Create a request
+ var request = new HTMLHttpReqest();
+ 
+     //Capture the response and store it in a variable
+ request.onreadystatechange = function (){
+     if (request.readystate === XMLHttpRequest.DONE){
+         //Take some action
+       if(request.status === 200){  
+        var counter = request.responseText
+        var span= document.getElementById('count');
+        span.innerHTML=counter.toString();
     
-    //Capture the response and store it in a variable
     
-    //Render the variable in the correct span
-    counter = counter+1;
-    var span= document.getElementById('count');
-    span.innerHTML=counter.toString();
-    
-}
+     }
+     //Not done yet
+ } ;  
+   
+    //Make the request
+    request.open('GET', 'http://sanvenkat21072017.imad.hasura-app.io/counter')
+    request.send(null);
+};
 
 
 
