fc2566d00e9744585f9d19ba67b5f572685e4d21
V3ENKAT <30932829+V3ENKAT@users.noreply.github.com>
2017-08-15 10:41:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -36,7 +36,7 @@ button.onclick=function (){
     }   
    
     //Make the request
-    request.open('GET', 'http://sanvenkat21072017.imad.hasura-app.io/counter',true)     
+    request.open('GET', 'http://sanvenkat21072017.imad.hasura-app.io/counter',true);     
     request.send(null);
 };
 
