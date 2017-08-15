f14fd6d14c51dafa1ecafe1144a91e56bceb9a9d
V3ENKAT <30932829+V3ENKAT@users.noreply.github.com>
2017-08-15 12:40:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -41,19 +41,17 @@ button.onclick=function (){
 };
 //submit name
 var nameInput = document.getElementById('name');
- name= nameInput.value;
+var name= nameInput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
 //Make a req.to the server and send the name.
 
 //capture a list of names and render it as a list.
 var names = ['name1', 'name2', 'name3','name4'];
-var list =' ';
+var list ='';
 for (var i=0; i <name.length; i++){
     list += '<li>'+names[i]+'</li>'; 
-    var ul=document.getElementById('namelist');
+   }
+   var ul=document.getElementById('namelist');
     ul.innerHTML=list;
-}
- ul=document.getElementById('namelist');
-ul.innerHTML=list;
 };
