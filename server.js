9a8a095839c82215f9db4a350642d01dbd227b6f
V3ENKAT <30932829+V3ENKAT@users.noreply.github.com>
2017-08-15 14:51:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ var counter = 0;
 app.get('/counter', function (req,res){counter = counter + 1;
     res.send(counter.toString());
 });
-var names=();
+var names=[];
 app.get('/submit-name',function(req,res){//URL:/submit-name?name-xxxxx
     //Get the name from the request
     var name=req.query.name; //T000
