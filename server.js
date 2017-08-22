e7075b4bbf85508eaa8c054a2d6ec00dba08ba19
V3ENKAT <30932829+V3ENKAT@users.noreply.github.com>
2017-08-22 21:34:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ var Pool = require ('pg').Pool;
 var config = {
     user: 'sanvenkat21072017'
     database: 'sanvenkat21072017'
-    port: 'db.imad.hasura-app.io' 
-    host: '5432'
+    port: '5432'
+    host: 'db.imad.hasura-app.io' 
     passowrd: process.env.DB_PASSWORD
 };
 var app = express();
