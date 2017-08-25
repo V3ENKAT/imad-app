9cc6a6e226218f7978c17726ebd5f8d40c1eb1aa
V3ENKAT <30932829+V3ENKAT@users.noreply.github.com>
2017-08-26 00:27:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -91,7 +91,8 @@ app.get('/', function (req, res) {
   app.get('/test-db', function (req, res){
       //make a select request
       //return a reponse with the results 
-        pool.query('SELECT * FROM "Article Table",function (err,result) {
+        pool.query(select * from ,function (err,result) 
+        {
             if(err){
                 res.status(500).send(err.toString());
             } else {
