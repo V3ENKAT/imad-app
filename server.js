0cf487d8167e3ab927c3c94bbd58a868194b8cfa
V3ENKAT <30932829+V3ENKAT@users.noreply.github.com>
2017-08-30 21:38:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var config = {
     database: 'sanvenkat21072017',
     host: 'db.imad.hasura-app.io',
     port: '5432',
-    password: 'process.env.DB_PASSWORD',
+    password:process.env.DB_PASSWORD,
 };
 var app = express();
 app.use(morgan('combined'));
