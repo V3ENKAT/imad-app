88a78619436f87bd8a5261a623bd9c1b2297c0ff
V3ENKAT <30932829+V3ENKAT@users.noreply.github.com>
2017-09-01 23:14:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -129,11 +129,16 @@ res.status(404).send('Article not found');
         } else { 
             var articleData=result.rows[0];
             res.send(createTemplate(articleData));
-            
         }}
         
     });
- 
+    
+    
+});
+            
+            
+        
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
