0f72620b22bc77f5cba0a8ac5c8b0daf47f41aa5
V3ENKAT <30932829+V3ENKAT@users.noreply.github.com>
2017-09-04 19:47:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -118,9 +118,9 @@ app.get('/articles/:articleName', function (req, res) {
     //articleName --- article-one
     //artilces[articleName]--- {} content object for article one
   //  var articleName = req.params.articleName;
-
+    // SELECT * FROM article WHERE title = 'article-one'
     
-    pool.query("SELECT * FROM article WHERE title = "+ req.params.articleName, function(err, result){
+    pool.query("SELECT * FROM article WHERE title = ' + req.params.articleName + '", function(err, result){
         if (err){
                 res.status(500).send(err.toString());
         }else {
